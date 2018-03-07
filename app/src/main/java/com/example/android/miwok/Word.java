c0a3caabac19d8995e65bb5cab005654fd6958db
Fabien Boismoreau <fabien.boismoreau@gmail.com>
2018-03-07 21:50:43 +0100

    feat: Modify Word object to add an image resource id

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -2,6 +2,7 @@ package com.example.android.miwok;
 
 /**
  * Created by Fabien Boismoreau on 19/02/2018.
+ * <p/>
  */
 
 public class Word {
@@ -10,9 +11,17 @@ public class Word {
 
     private String mDefaultTranslation;
 
+    private int mImageResourceId;
+
     public Word(String defaukt, String miwok) {
         mMiwokTranslation = miwok;
         mDefaultTranslation = defaukt;
+    }
+
+    public Word(String mMiwokTranslation, String mDefaultTranslation, int mImageResourceId) {
+        this.mMiwokTranslation = mMiwokTranslation;
+        this.mDefaultTranslation = mDefaultTranslation;
+        this.mImageResourceId = mImageResourceId;
     }
 
     public String getMiwokTranslation() {
@@ -21,5 +30,9 @@ public class Word {
 
     public String getdefaultTranslation() {
         return mDefaultTranslation;
+    }
+
+    public int getImageResourceId() {
+        return mImageResourceId;
     }
 }
