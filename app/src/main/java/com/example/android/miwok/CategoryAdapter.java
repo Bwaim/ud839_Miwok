934d78c9960dfa9393ba3da0e5e955c29d1f0c25
Fabien Boismoreau <fabien.boismoreau@gmail.com>
2018-03-11 22:33:50 +0100

    feat: Add ViewPager

---
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -0,0 +1,41 @@
+package com.example.android.miwok;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Fabien Boismoreau on 11/03/2018.
+ * <p>
+ */
+
+public class CategoryAdapter extends FragmentPagerAdapter {
+
+    public CategoryAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    /**
+     * Return the Fragment associated with a specified position.
+     *
+     * @param position
+     */
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new NumbersFragment();
+        } else if (position == 1) {
+            return new FamilyFragment();
+        } else if (position == 2) {
+            return new ColorsFragment();
+        } else {
+            return new PhraseFragment();
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
